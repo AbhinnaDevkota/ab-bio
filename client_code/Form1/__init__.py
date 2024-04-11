c14e16aae302004385fcc46d5e06d51ac34d3718
abhinna.devkota@gmail.com <abhinna.devkota@gmail.com>
2024-04-11 05:38:53 +0000

    Edited Form1, Form2, Form3, Form4 and 2 more

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,7 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
 import anvil.users
 import anvil.server
 import anvil.tables as tables
@@ -13,18 +15,14 @@ class Form1(Form1Template):
 
     # Any code you write here will run before the form opens.
 
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    email = str(self.text_box_1)
-    hobby = str(self.text_area_1)
-    anvil.server.call('submit' , email = email, hobby = hobby)
-    Notification("your response has been recorded").show()
+  
 
   def button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('Form2')
 
-  
+    anvil.users.login_with_form()
+    anvil.server.call('print_my_permissions')
 
   
     
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,3 +1,5 @@
+import anvil.google.auth, anvil.google.drive, anvil.google.mail
+from anvil.google.drive import app_files
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
@@ -17,9 +19,20 @@ import anvil.server
 #   return 42
 @anvil.server.callable
 def submit (email, hobby):
- app_tables.table_1.add_row(email=email,hobby=hobby)
+ app_tables.table_1.add_row(hobby=hobby)
 
 @anvil.server.callable
 def submit (blog):
   app_tables.table_1.add_row(blog=blog)
-  
+
+@anvil.server.callable
+def print_my_permissions():
+  super_user = 'ryan@anvil.works'
+  if anvil.users.get_user() is None:
+    print("Nobody is logged in.")
+  elif anvil.users.get_user()['email'] == super_user:
+    print(f"{super_user} is allowed to see this.")
+  else:
+    print("This path is for minimum-access users.")
+
+
